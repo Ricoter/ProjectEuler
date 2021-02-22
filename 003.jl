3ae2155e8a9f695ca491d5f9916174f8c8cb382b
ricoter <rico.vanmidde@gmail.com>
2021-02-22 01:50:07 +0100

    p3

---
+++ 003.jl
@@ -0,0 +1,23 @@
+#=
+The prime factors of 13195 are 5, 7, 13 and 29.
+
+What is the largest prime factor of the number 600851475143 ?
+=#
+
+function largestPrime(n::Int)
+    """Return largest prime factor for a natural number."""
+    f = 2
+    while n%f == 0
+        n /= f
+    end
+
+    f += 1 
+    while n > 1
+        n%f==0 ? n÷=f : f+=2
+    end
+    return f
+end
+
+
+z(n,f=2) = n==1 ? f : (n%f==0 ? z(n÷f,f) : z(n,f+1))
+z(600851475143), largestPrime(600851475143)

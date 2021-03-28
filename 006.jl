e0ee03e800962dbecfb5a6beb179f4d89f8fb679
ricoter <rico.vanmidde@gmail.com>
2021-03-29 01:33:28 +0200

    add math solution

--- 006.jl
+++ 006.jl
@@ -1,1 +1,11 @@
-sum(1:100)^2 - sum((1:100).^2)
+using BenchmarkTools
+
+sumsquares1(N) = sum(1:N)^2 - sum((1:N).^2)
+
+function sumsquares(N=100)
+    sum = N * (N + 1) / 2
+    sumsq = (2 * N + 1) * (N + 1) * N / 6
+    return sum^2 - sumsq
+end
+
+@benchmark sumsquares(1000000); @benchmark sumsquares1(1000000)
